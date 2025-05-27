361946bbeac0873f07aaa557bb7ef651e8a67720
Ryo Kasai <ryo.kasai19@gmail.com>
2025-05-27 16:42:50 +0900

    mark

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from langchain.chains.retrieval_qa.base import RetrievalQA
 from dotenv import load_dotenv # this gets my OPEN ai API code
 load_dotenv() 
 print("✅ App started")
+print("✅ KEY LOADED:", os.getenv("OPENAI_API_KEY"))
 
 st.set_page_config(page_title="Ask Your Markdown", page_icon="📘")
 st.title("📘 Ask Your Markdown")
